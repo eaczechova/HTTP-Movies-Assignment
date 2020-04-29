43d9cc99083815f2887f2f2dcded233120c88440
Ewa Czech <ewa.czech@hotmail.com>
2020-04-29 02:58:37 +0200

    add routing and update functionality

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UpdateMovie = (props) => {
+	const [movie, setMovie] = useState();
+
+	// useEffect is used to avoid initialization of useState() before movies gets populated
+	// very first time the component is rendered props is undefined
+	useEffect(() => {
+		const selectedMovie = props.movies.find((movie) => {
+			return `${movie.id}` === props.match.params.id;
+		});
+		if (selectedMovie) {
+			setMovie(selectedMovie);
+		}
+	}, [props.movies, props.match.params.id]);
+
+	const changeHandler = (e) => {
+		let value = e.target.value;
+		setMovie({ ...movie, [e.target.name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+			.then((res) => {
+				props.history.push('/');
+				props.getMovieList();
+			})
+			.catch((err) => console.log(err));
+	};
+
+	return (
+		<div className="movie-card">
+			{movie && (
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						value={movie.title}
+						name="title"
+						onChange={changeHandler}
+					/>
+					<input
+						type="text"
+						value={movie.director}
+						name="director"
+						onChange={changeHandler}
+					/>
+					<input
+						type="text"
+						value={movie.metascore}
+						name="metascore"
+						onChange={changeHandler}
+					/>
+
+					<input
+						className="movie-star"
+						value={movie.stars}
+						name="stars"
+						onChange={changeHandler}
+					/>
+
+					<button>Save Changes</button>
+				</form>
+			)}
+		</div>
+	);
+};
+
+export default UpdateMovie;
